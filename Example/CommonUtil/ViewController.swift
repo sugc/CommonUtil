9a3ae942138c9e9743170a35c5c8966d71032fb1
sugc <2528397406@qq.com>
2021-01-24 21:23:35 +0800

    Initial commit

---
+++ Example/CommonUtil/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CommonUtil
+//
+//  Created by sugc on 01/24/2021.
+//  Copyright (c) 2021 sugc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
